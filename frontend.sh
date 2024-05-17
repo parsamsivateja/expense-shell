74f22ab0aa6106f8fba4e63f33902b91b93e9194
sivateja <sivateja25101997@gmail.com>
2024-05-17 20:43:30 +0530

    re ordering the code lines

--- frontend.sh
+++ frontend.sh
@@ -1,5 +1,5 @@
 dnf install nginx -y
-
+cp expense.conf /etc/nginx/default.d/expense.conf
 systemctl enable nginx
 systemctl start nginx
 
@@ -10,6 +10,6 @@ curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zi
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
 
-cp expense.conf /etc/nginx/default.d/expense.conf
+
 
 systemctl restart nginx
