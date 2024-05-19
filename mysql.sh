34ab38ff590233ec71a6a91d4f025243aa9307fb
sivateja <sivateja25101997@gmail.com>
2024-05-19 12:56:00 +0530

    writing echo commands

--- mysql.sh
+++ mysql.sh
@@ -1,10 +1,16 @@
+source common.sh
+echo Disable Mysql 8 version
 dnf module disable mysql -y
 
+echo copy Mysql repo file
 cp mysql.repo /etc/yum.repos.d/mysql.repo
 
+echo install Mysql server
 dnf install mysql-community-server -y
 
+echo start mysql service
 systemctl enable mysqld
 systemctl start mysqld
 
+echo setup root password
 mysql_secure_installation --set-root-pass ExpenseApp@1
