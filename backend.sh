c206e2f2bbc960817d2d97d9ae79cc2215e978aa
Sivateja <sivateja25101997@gmail.com>
2025-03-07 12:30:29 +0530

    DnS Name

--- backend.sh
+++ backend.sh
@@ -16,4 +16,4 @@ systemctl daemon-reload
 systemctl enable backend
 systemctl start backend
 dnf install mysql -y
-mysql -h shell.172.31.35.63 -uroot -pExpenseApp@1 < /app/schema/backend.sql
+mysql -h 172.31.35.63 -uroot -pExpenseApp@1 < /app/schema/backend.sql
